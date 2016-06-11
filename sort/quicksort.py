79e9f98cb868e7c14037acd88450a4283999fb41
Yolanda Kokkinou <yiola8c@gmail.com>
2016-06-11 15:43:25 +0300

    Partition implementation

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -3,7 +3,15 @@
 # definition of the partition subroutine
 
 def partition(a, l, r):
-    
+    # pivot first element of the array
+    pivot = a[l]
+    index = l
+    for i in range(l + 1, r:
+        if a[i] <= pivot:
+            index++
+            swap(a[i], a[index])
+    swap(a[pivot], a[index])
+    return index
 
-def quicksort(a,l,r):
-    q = partition()
+def quicksort(a, l, r):
+    q = partition(a, l, r)
