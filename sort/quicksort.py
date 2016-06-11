b78ee7a35e63ef00a906129a74b66f76393c3d20
Yolanda Kokkinou <yiola8c@gmail.com>
2016-06-11 16:06:37 +0300

    Solved some bugs in recursion

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -13,12 +13,18 @@ def partition(a, l, r):
     index = l
     for i in range(l + 1, r):
         if a[i] <= pivot:
-            index+=1
+            index+= 1
             swap(a[i], a[index])
-    swap(a[pivot], a[index])
+    swap(pivot, a[index])
     return index
 
 def quicksort(a, l, r):
     q = partition(a, l, r)
-    quicksort(a, l, q)
+    quicksort(a, l, q - 1)
     quicksort(a, q + 1, r)
+
+b = [4, 3, 5, 7]
+i = 0
+quicksort(b, i, len(b) - 1)
+for j in range(i, len(b) - 1):
+    print b[j],
