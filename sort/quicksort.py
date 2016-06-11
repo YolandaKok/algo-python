c0d463599b22565c0489d330f83b26f5098216b0
Yolanda Kokkinou <yiola8c@gmail.com>
2016-06-12 01:20:10 +0300

    some experiments to change the bug

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -9,6 +9,7 @@ def partition(a, l, r):
     for i in range(l + 1, r):
         if a[i] <= pivot:
             index += 1
+            # tuple packing instead of swap function
             a[i], a[index] = a[index], a[i]
     a[index], a[l] = a[l], a[index]
     return index
@@ -19,7 +20,7 @@ def quicksort(a, l, r):
         quicksort(a, l, q - 1)
         quicksort(a, q + 1, r)
 
-b = [4, 3, 5, 7]
+b = [4, 3, 5, 7, 2]
 i = 0
 quicksort(b, i, len(b) - 1)
 for j in range(i, len(b) - 1):
