129dfd7b27f3e0a34025ef430b6e1b28168cbe7b
Yolanda Kokkinou <yiola8c@gmail.com>
2016-06-12 14:52:38 +0300

    Fixed the bug in for loop

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -3,15 +3,15 @@
 # definition of the partition subroutine
 
 def partition(a, l, r):
-    # pivot first element of the array
+# pivot first element of the array
     pivot = a[l]
     index = l
-    for i in range(l + 1, r):
+    for i in range(l + 1, r + 1):
         if a[i] <= pivot:
             index += 1
             # tuple packing instead of swap function
             a[i], a[index] = a[index], a[i]
-    a[index], a[l] = a[l], a[index]
+    a[l], a[index] = a[index], a[l]
     return index
 
 def quicksort(a, l, r):
@@ -19,9 +19,3 @@ def quicksort(a, l, r):
         q = partition(a, l, r)
         quicksort(a, l, q - 1)
         quicksort(a, q + 1, r)
-
-b = [8, 1, 3, 7, 2]
-i = 0
-quicksort(b, i, len(b) - 1)
-for j in range(i, len(b) - 1):
-    print b[j]
