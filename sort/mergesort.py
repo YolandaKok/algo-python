ab41957948bfe9fc731fa93aec1ea9940b3716d9
Yolanda Kokkinou <yiola8c@gmail.com>
2016-06-16 21:16:06 +0300

    Completed the merge function

--- sort/mergesort.py
+++ sort/mergesort.py
@@ -16,11 +16,45 @@ def Merge(a, l, q, r):
     # bound for the second temporary array
     capacity2 = r - q
     # initialize the temporary arrays
-    for i in range(0, capacity1):
-        left[i] = 0
-
-    for j in range(0, capacity2):
-        right[j] = 0
+    left = [0] * capacity1
+    right = [0] * capacity2
 
     # copy the elements of list a to the subarrays
-            
+    for i in range(0, capacity1):
+        left[i] = a[l + i]
+
+    for j in range(0, capacity2):
+        right[j] = a[q + 1 + j]
+
+    # indexes for arrays
+    i = 0
+    j = 0
+    k = l
+
+    while i < capacity1 and j < capacity2:
+        # choice of the first element in list a
+        if left[i] <= right[j]:
+            a[k] = left[i]
+            i += 1
+        else:
+            a[k] = right[j]
+            j += 1
+        k += 1
+
+    # if list one has not ended
+    while i < capacity1:
+        a[k] = left[i]
+        i +=1
+        k += 1
+
+    while j < capacity2:
+        a[k] = right[j]
+        j += 1
+        k += 1
+
+
+a = [2, 1, 10, 9]
+Mergesort(a, 0, 3)
+
+for i in range(0, 4):
+    print a[i]
